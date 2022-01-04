7c20d926d3e78fdf19e0285c35dcd97f860409ac
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-01-03 16:46:44 -0800

    :memo: Disabled AOV Booster

--- build/build.js
+++ build/build.js
@@ -1,6 +1,5 @@
 import Alpine from 'alpinejs/src';
 import * as Daybreak from './daybreak';
-import AOVBooster from './aovbooster';
 import Trees from './alpine';
 
 
@@ -8,8 +7,5 @@ window.Daybreak = Daybreak;
 window.Alpine = Alpine;
 
 Trees.forEach((tree) => Alpine.plugin(tree));
-AOVBooster()
 Alpine.start();
 Daybreak.RIAS.start();
-
-// AOVBooster();
